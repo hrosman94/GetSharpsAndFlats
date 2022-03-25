891132b13a15a10c881af8d88fdda137d6c20db6
Healy Rosman <98075737+hrosman94@users.noreply.github.com>
2022-03-25 18:11:14 -0500

    Update Main.hs
    - Changed print message "C has no sharps/flats" to "CN has no sharps/flats" for clarification
    - Changed final result to include "Major" for clarification

--- Main.hs
+++ Main.hs
@@ -17,7 +17,7 @@ getSharpsAndFlats (letter, anySFNs) =
           let orderOfFlats = ["B","E","A","D","G","C","F"]
           let yourFlats = take (abs(howMany)) (cycle orderOfFlats)
           let toString = show(length (yourFlats))
-          putStrLn(letter++anySFNs++" has "++toString++" flats. The flats are:")
+          putStrLn(letter++anySFNs++" Major has "++toString++" flats. The flats are:")
           print(yourFlats)
       else print("ERROR: flat key should always return negative number")
   else if anySFNs == "#"
@@ -29,7 +29,7 @@ getSharpsAndFlats (letter, anySFNs) =
       then do 
         let yourSharps = take howMany (cycle orderOfSharps)
         let toString = show(length (yourSharps))
-        putStrLn(letter++anySFNs++" has "++toString++" sharp(s). The sharp(s) is(are):")
+        putStrLn(letter++anySFNs++" Major has "++toString++" sharp(s). The sharp(s) is(are):")
         print(yourSharps)
       else print("ERROR: sharp key should always return positive number")
   else if anySFNs == "N"
@@ -41,16 +41,16 @@ getSharpsAndFlats (letter, anySFNs) =
           let orderOfFlats = ["B","E","A","D","G","C","F"]
           let yourFlats = take (abs(howMany)) (cycle orderOfFlats)
           let toString = show(length (yourFlats))
-          putStrLn(letter++anySFNs++" has "++toString++" flat(s). The flat(s) is(are):")
+          putStrLn(letter++anySFNs++" Major has "++toString++" flat(s). The flat(s) is(are):")
           print(yourFlats)
       else if howMany > 0 
         then do 
           let yourSharps = take howMany (cycle orderOfSharps)
           let toString = show(length (yourSharps))
-          putStrLn(letter++anySFNs++" has "++toString++" sharp(s). The sharps is(are):")
+          putStrLn(letter++anySFNs++" Major has "++toString++" sharp(s). The sharps is(are):")
           print(yourSharps)
       else do
-          print("C has no sharps/flats")
+          print("CN Major has no sharps/flats")
   else do
     print("ERROR: Invalid entry in anySFNs")
 
@@ -58,7 +58,7 @@ main = do
        let letters = ["A","B","C", "D", "E", "F", "G"]
        let sharpsFlatsOrNats = ["#", "flat", "N"]
        
-       putStrLn "What key in the major scale do you want sharps/flats for?"
+       putStrLn "What key in the Major scale do you want sharps/flats for?"
        input <- getLine
        putStrLn ("You want sharps/flats for: "++ input)
 
