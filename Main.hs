2a10cd237c92f6990b9e7935b4b791e014bf7558
Healy Rosman <98075737+hrosman94@users.noreply.github.com>
2022-03-25 17:34:05 -0500

    Update Main.hs
    Added lists containing order of sharps/flats as well as lists associated with numbers representing the number of sharps/flats for sharp, flat, and natural keys. Is now able to now return where the sharps/flats are located on staff paper as well as the number.

--- Main.hs
+++ Main.hs
@@ -1,54 +1,58 @@
 import Data.List
+import Data.List(intercalate)
 import System.IO
 
 getSharpsAndFlats (letter, anySFNs) = 
  do
-  if (letter == "F" && anySFNs == "flat")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 8 flats")
-  else if (letter == "C" && anySFNs == "flat")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 7 flats")
-  else if (letter == "G" && anySFNs == "flat")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 6 flats")
-  else if (letter == "D" && anySFNs == "flat")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 5 flats")
-  else if (letter == "A" && anySFNs == "flat")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 4 flats")
-  else if (letter == "E" && anySFNs == "flat")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 3 flats")
-  else if (letter == "B" && anySFNs == "flat")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 2 flats")
-  else if (letter == "F" && anySFNs == "N")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 1 flat")
-  else if (letter == "C" && anySFNs == "N")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 0 sharps or flats")
-  else if (letter == "G" && anySFNs == "N")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 1 sharps")
-  else if (letter == "D" && anySFNs == "N")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 2 sharps")
-  else if (letter == "A" && anySFNs == "N")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 3 sharps")
-  else if (letter == "E" && anySFNs == "N")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 4 sharps")
-  else if (letter == "B" && anySFNs == "N")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 5 sharps")
-  else if (letter == "F" && anySFNs == "#")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 6 sharps")
-  else if (letter == "C" && anySFNs == "#")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 7 sharps")
-  else if (letter == "G" && anySFNs == "#")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 8 sharps")
-  else if (letter == "D" && anySFNs == "#")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 9 sharps")
-  else if (letter == "A" && anySFNs == "#")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 10 sharps")
-  else if (letter == "E" && anySFNs == "#")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 11 sharps")
-  else if (letter == "B" && anySFNs == "#")
-    then putStrLn("    "++letter ++ anySFNs ++ " has 12 sharps")
-  else 
-    putStrLn("ERROR: No condition met in getSharpsAndFlats")
+  let orderOfSharps = ["F","C","G","D","A","E","B"]
+  let n = [-1,0..5]
 
-  putStrLn("     **terminating**")
+  if anySFNs == "flat"
+    then do 
+      let flatsList = [x-7 | x <- n]
+      let Just i = elemIndex letter orderOfSharps
+      let howMany = flatsList !! i
+      if howMany < 0
+        then do 
+          let orderOfFlats = ["B","E","A","D","G","C","F"]
+          let yourFlats = take (abs(howMany)) (cycle orderOfFlats)
+          let toString = show(length (yourFlats))
+          putStrLn(letter++anySFNs++" has "++toString++" flats. The flats are:")
+          print(yourFlats)
+      else print("ERROR: flat key should always return negative number")
+  else if anySFNs == "#"
+    then do 
+      let Just i = elemIndex letter orderOfSharps
+      let sharpsList = [x+7 | x <- n]
+      let howMany = sharpsList !! i
+      if howMany > 0
+      then do 
+        let yourSharps = take howMany (cycle orderOfSharps)
+        let toString = show(length (yourSharps))
+        putStrLn(letter++anySFNs++" has "++toString++" sharp(s). The sharp(s) is(are):")
+        print(yourSharps)
+      else print("ERROR: sharp key should always return positive number")
+  else if anySFNs == "N"
+    then do
+      let Just i = elemIndex letter orderOfSharps
+      let howMany = n !! i
+      if howMany < 0
+        then do 
+          let orderOfFlats = ["B","E","A","D","G","C","F"]
+          let yourFlats = take (abs(howMany)) (cycle orderOfFlats)
+          let toString = show(length (yourFlats))
+          putStrLn(letter++anySFNs++" has "++toString++" flat(s). The flat(s) is(are):")
+          print(yourFlats)
+      else if howMany > 0 
+        then do 
+          let yourSharps = take howMany (cycle orderOfSharps)
+          let toString = show(length (yourSharps))
+          putStrLn(letter++anySFNs++" has "++toString++" sharp(s). The sharps is(are):")
+          print(yourSharps)
+      else do
+          print("C has no sharps/flats")
+  else do
+    print("ERROR: Invalid entry in anySFNs")
 
 main = do
        let letters = ["A","B","C", "D", "E", "F", "G"]
@@ -56,7 +60,7 @@ main = do
        
        putStrLn "What key in the major scale do you want sharps/flats for?"
        input <- getLine
-       putStrLn ("  You want sharps/flats for: "++ input)
+       putStrLn ("You want sharps/flats for: "++ input)
 
        let letter = take 1 input
        let isValid = elem letter letters
@@ -65,8 +69,7 @@ main = do
        let hasSFNs = elem anySFNs sharpsFlatsOrNats
 
        if (isValid && hasSFNs)
-         then do 
-              putStrLn("   Key is valid") 
+         then do  
               getSharpsAndFlats(letter, anySFNs)
          else do
               putStrLn("ERROR: Key is invalid")
